9277a1f16caa124708cf171ac19c0b3d354f813e
Vishnugit26 <155157978+Vishnugit26@users.noreply.github.com>
2023-12-29 17:57:17 +0530

    Add files via upload

---
+++ js task30/index.js
@@ -0,0 +1,13 @@
+const myImg = document.getElementById("imgid")
+
+document.getElementById("buttonsa").addEventListener("click",function(){
+    myImg.src="/apclpt.jpg";
+})
+
+document.getElementById("buttonsb").addEventListener("click",function(){
+    myImg.src="/10000-bc.jpg";
+})
+
+document.getElementById("buttonsc").addEventListener("click",function(){
+    myImg.src="/avatar.jpg";
+})
